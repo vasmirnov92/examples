fdb803bf589d5f5b761bea1fbea9556a6b101779
Vasiiy <vasya.smirnoff@mail.ru>
2023-01-13 17:00:10 +0300

    example003 summ

---
+++ example003_summ/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.WriteLine("Введите первое число");
+//int firstNumber = Console.ReadLine();
+//Console.WriteLine("Введите второе число");
+//int secondNumber = Console.ReadLine();
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine (result);
