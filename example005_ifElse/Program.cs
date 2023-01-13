4b5aed79781504671cb48e20aeac589b1c7a3047
Vasiiy <vasya.smirnoff@mail.ru>
2023-01-14 00:53:46 +0300

    example005 if else

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "настя")
+{
+    Console.WriteLine("Привет, киса любимая!");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(username);
+}
