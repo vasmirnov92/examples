8cfc1a9dfa43aaed38b71448b39ff009d8d60fcc
Vasiiy <vasya.smirnoff@mail.ru>
2023-01-14 03:40:55 +0300

    example007 triangle serpinsk

---
+++ example007_triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1 , yb = 20,
+    xc = 20, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("=");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("~");
