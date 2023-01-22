8605a58cbd09f6284934923c96f32528a6fce638
Vasiiy <vasya.smirnoff@mail.ru>
2023-01-22 13:40:42 +0300

    add method array

---
+++ example010_methodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {18, 52, 43, 54, 85, 76, 67, 85, 84};
+
+int n = array.Length;
+int find = 85;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+    Console.WriteLine (index);
+    break;
+    }
+   // else Console.WriteLine("такого элемента массива нет");
+    index++;
+}
