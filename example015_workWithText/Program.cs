52082e7d6893e8f06381af43d88ee6f81cabd964
Vasiiy <vasya.smirnoff@mail.ru>
2023-01-29 18:47:19 +0300

    add work with text

---
+++ example015_workWithText/Program.cs
@@ -0,0 +1,36 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// при работе со строками можно обратиться непосредственно к символу, например
+// string s = "qwerty"
+//             012345
+// s[3] // r        // обращаемся к третьему символу и получаем "r"
+
+string replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;             //получаем длину строки 
+    for (int i=0; i<length; i++)
+    {
+        if (text[i] == oldValue) result = result +  $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+string newText1 = replace(newText, 'с', 'С');
+Console.WriteLine(newText1);
+Console.WriteLine();
+string newText2 = replace(newText1, 'к', 'К');
+Console.WriteLine(newText2);
